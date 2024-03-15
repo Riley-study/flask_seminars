501d231cf1c77291342bf6ff7891ac5ba9500928
Riley-study <120293475+Riley-study@users.noreply.github.com>
2024-03-15 13:58:16 +0500

    hw 4

--- project/hw_4/task_01_threads.py
+++ project/hw_4/task_01_threads.py
@@ -10,6 +10,7 @@ import threading
 import requests
 import time
 
+
 urls = [
     'https://i.pinimg.com/736x/be/39/7c/be397c91b8026b17f5f8a6ed98e23e9e.jpg'
     'https://media.contented.ru/wp-content/uploads/2022/12/3-1024x681.png',
@@ -17,7 +18,6 @@ urls = [
     'https://www.img2go.com/assets/dist/sample-files/img/convert_to_jpg.png',
     'https://cs6.livemaster.ru/storage/c1/ad/0fb10c243c18873254c4ac194epd.jpg',
     'https://habrastorage.org/r/w1560/webt/xh/7m/8n/xh7m8nokzhgfqixb00fnwcmkthm.jpeg',
-    'https://dkrotov.com/delight.webpconverter/webp_converter_test_image.jpg'
 ]
 
 
@@ -34,6 +34,7 @@ def download_picture(url_picture):
 
 threads = []
 start_time = time.time()
+
 for url in urls:
     thread = threading.Thread(target=download_picture, args=[url])
     threads.append(thread)
